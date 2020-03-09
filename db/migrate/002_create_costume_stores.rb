70569750391ce225e82c896ce13f498dd46decbd
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-09 01:13:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
-# Create your costume_stores migration here
+class CreateCostumesStore < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.string :business
+      t.datetime :opening
+      t.datetime :closing
+    end
+  end
+  
+end  
